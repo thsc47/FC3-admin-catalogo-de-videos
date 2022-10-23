fdb1c7178ea27d9bf6ffef3860fb4cf9aa66d687
thsc47 <thsc47@gmail.com>
2022-10-23 14:26:21 -0300

    sync scaffold

---
+++ src/main/java/com/admin/catalogo/fc3admincatalogodevideos/Fc3AdminCatalogoDeVideosApplication.java
@@ -0,0 +1,13 @@
+package com.admin.catalogo.fc3admincatalogodevideos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fc3AdminCatalogoDeVideosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fc3AdminCatalogoDeVideosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/admin/catalogo/fc3admincatalogodevideos/Fc3AdminCatalogoDeVideosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.admin.catalogo.fc3admincatalogodevideos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fc3AdminCatalogoDeVideosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
